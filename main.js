623763f59e8247d63ba10994f8bf801af4f2e30a
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2020-04-04 15:38:10 -0300

    Switch

--- main.js
+++ main.js
@@ -169,3 +169,19 @@
 // const color = x > 10 ? 'red' : 'blue';
 
 // console.log(color);
+
+const x = 5;
+
+const color = x > 10 ? 'red' : 'blue';
+
+switch(color){
+  case 'red':
+    console.log('color is red');
+    break;
+  case  'blue':
+    console.log('color is blue');
+    break;
+  default: 
+    console.log('color is NOT red or blue');
+    break;
+}
